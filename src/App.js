4077909198658a1bc1c8dc4a383d06c0a52c9f5c
YungGooby <liam_toole@hotmail.co.uk>
2021-09-02 11:53:23 +0100

    fetch request done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Test Header</h1>
-      </header>
-    </div>
-  );
+    const [cats, setCats] = useState([]);
+
+    const getCats = async () => {
+        // for (let i = 0; i < 9; i += 1) {
+            const response = await fetch(
+                "https://api.thecatapi.com/v1/images/search?limit=10"
+            );
+            const data = await response.json();
+            setCats(data);
+        // }
+    };
+    useEffect(() => {
+        getCats();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Cats4Sale</h1>
+                {cats.map((cat) => (
+                    <img src={cat.url} alt=""></img>
+                ))}
+            </header>
+        </div>
+    );
 }
 
 export default App;
